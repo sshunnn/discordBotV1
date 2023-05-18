3feeb76a02a6a074ade5f676be227084cfcf8ee5
sshunnn <piassy.c0mp@gmail.com>
2023-05-19 02:22:12 +0900

    聞いている音楽の送信とリファクタ

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv');
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const cron = require('node-cron');
+const { Client, Events, GatewayIntentBits, cleanCodeBlockContent } = require('discord.js');
 const client = new Client({
     'intents': [
         GatewayIntentBits.Guilds,
@@ -9,6 +10,16 @@ const client = new Client({
     ]
 });
 dotenv.config();
+
+// cron job 3時間ごとに定期実行
+// TODO: messageCreateではメッセージ送信時にしか発火しないため、cronで動かすとエラーが起きるため
+//       fetchで別途messageを取るようにする。
+
+// cron.schedule('0 0 */3 * * *', () => client.on("messageCreate", (message) => {
+//     if (message.author.id === process.env.BOT_ID) {
+//         message.delete();
+//     }
+// }));
 
 
 client.on('ready', () => {
@@ -26,19 +37,52 @@ client.once(Events.ClientReady, c => {
  */
 client.on('presenceUpdate', (oldPresence, newPresence) => {
     const member = newPresence.member;
-    const userStatus = newPresence.clientStatus;
-    const userTerminalInfo = !!userStatus.desktop ? 'パソコン' : 'スマートフォン'
+    const isUser = process.env.USER_ID.includes(newPresence.userId)
+    const isUserStatus = newPresence.status
+    const isMobile = !!newPresence.clientStatus.mobile
+    let statusMessage
 
-    if (Number(newPresence.userId) === Number(process.env.USER_ID)) {
-        // ユーザーのクライアント情報を取得
-        if (newPresence.status === 'online' || newPresence.status === 'offline') {
-            let statusMessage = (userStatus.desktop === 'online' || userStatus.mobile === 'online') ? `${member.user.username} が${userTerminalInfo}でオンラインになりました。\n` : `${member.user.username} がオフラインになりました。\n`;
-            client.channels.cache.get(process.env.BOT_CHAT_ROOM_ID).send(statusMessage);
-        } else if(newPresence.status === 'idle') {
-            statusMessage = `${member.user.username} はどっかに逝きました。\n`
-            client.channels.cache.get(process.env.BOT_CHAT_ROOM_ID).send(statusMessage);
-        }
+    //　statusが同じ場合はメッセージを送信しない
+    if (!!oldPresence && oldPresence.status === newPresence.status) {
+        return
+    } else if (isUser && isUserStatus === 'online') {
+        statusMessage = `${member.user.username} が${isMobile ? 'スマートフォン' : 'パソコン'}でオンラインになりました。\n`;
+    } else if (isUser && isUserStatus === 'offline') {
+        statusMessage = `${member.user.username} がオフラインになりました。\n`
+    } else if (isUser && newPresence.status === 'idle') {
+        statusMessage = `${member.user.username} はどっかに逝きました。\n`
+    }
+
+    client.channels.cache.get(process.env.BOT_CHAT_ROOM_ID).send(statusMessage);
+});
+
+
+/**
+ * 聞いている曲をチャットに記録する
+ */
+client.on('presenceUpdate', (oldPresence, newPresence) => {
+    let isOldMusic = !!oldPresence.activities ? oldPresence.activities.find(info => info.name === 'Spotify') : null
+    let isNewMusic = !!newPresence.activities ? newPresence.activities.find(newInfo => newInfo.name === 'Spotify') : null
+
+    if (!!isNewMusic && !!isOldMusic && isOldMusic.details === isNewMusic.details) {
+        return
+    } else if (!!isNewMusic) {
+        let statusMessage = `${client.users.cache.get(newPresence.userId)}is playing MUSIC now... \n 曲名：${isNewMusic.details}　　歌手：${isNewMusic.state}`
+        client.channels.cache.get(process.env.BOT_MUSIC_HISTORY_ROOM_ID).send(statusMessage);
     }
 });
+
+/**
+ * BOTのメッセージを消す
+ * TODO：100件メッセージを取っているが、指定できるようにする
+ */
+client.on('messageCreate', async message => {
+    if (message.content === '!del') {
+        message.delete();
+        const messages = await message.channel.messages.fetch({ limit: 100 })
+        const filtered = messages.filter(message => message.author.id === process.env.BOT_ID)
+        message.channel.bulkDelete(filtered)
+    }
+})
 
 client.login(process.env.DISCORD_TOKEN);
